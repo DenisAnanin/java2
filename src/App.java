2f7095576e5550a532de14529a8c20586049e1d2
DenisAnanin <denis-anapa@yandex.ru>
2022-03-21 23:58:22 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Humen humen = new Humen(0, 0);
+        System.out.println(humen.getDistance());
+    }
+
+}
