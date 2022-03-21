5d00afb97e9a25b2f1c1f0544b9ccac2638d80ce
DenisAnanin <denis-anapa@yandex.ru>
2022-03-22 00:04:59 +0300

    Начало Домашнего задания Урок 1

---
+++ src/Humen.java
@@ -0,0 +1,17 @@
+public class Humen {
+    private int distance;
+    private int up;
+
+    public Humen(int distance, int up) {
+        this.distance = 100;
+        this.up = 1;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public int getUp() {
+        return this.up;
+    }
+}
