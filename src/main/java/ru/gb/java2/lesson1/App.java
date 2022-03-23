6c8ab4da9adbb17db400ba135a83862194a9e87c
DenisAnanin <denis-anapa@yandex.ru>
2022-03-24 00:21:43 +0300

    продолжение ДЗ Урок 1

--- src/main/java/ru/gb/java2/lesson1/App.java
+++ src/main/java/ru/gb/java2/lesson1/App.java
@@ -1,17 +1,30 @@
 package ru.gb.java2.lesson1;
 
 public class App {
+    public static Humen[] humen = new Humen[2];
     public static void main(String[] args) {
-        Humen humen = new Humen();
+
         Cat cat =new Cat();
         Robot robot =new Robot();
 
         Barrier barrier =new Barrier();
         Treadmill treadmill =new Treadmill();
+        makeSportsmen(humen);
+        runAndJamp(humen,barrier,treadmill);
+//        humen.jamp(barrier);
+//        humen.run(treadmill);
 
-        humen.jamp(barrier);
-        humen.run(treadmill);
-
+    }
+    public static void makeSportsmen(Humen[] humen) {
+        for (int i = 0; i < humen.length; i++) {
+            humen[i] = new Humen();
+        }
+    }
+    public static void runAndJamp(Humen[] humen, Barrier b, Treadmill t) {
+        for (Humen h : humen) {
+            h.jamp (b);
+            h.run(t);
+        }
     }
 
 }
